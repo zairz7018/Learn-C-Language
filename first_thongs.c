0d6a44bdee04456c04f799dd4c18bc196110e436
zairz7018 <zairz7018@gmail.com>
2023-09-22 20:03:02 +0100

    committed

--- first_thongs.c
+++ first_thongs.c
@@ -145,4 +145,67 @@ int main() {
     return 0;
 }
 
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    // Exemple 1 : Calcul de la racine carrée
+    double nombre1 = 16.0;
+    double racine_carree = sqrt(nombre1);
+    printf("La racine carrée de %.2lf est %.2lf\n", nombre1, racine_carree);
+
+    // Exemple 2 : Calcul de la valeur absolue (entier)
+    int nombre2 = -7;
+    int valeur_absolue_entier = abs(nombre2);
+    printf("La valeur absolue de %d est %d\n", nombre2, valeur_absolue_entier);
+
+    // Exemple 3 : Utilisation de la fonction trigonométrique sin
+    double angle_degrees = 45.0;
+    double angle_radians = angle_degrees * M_PI / 180.0; // Conversion en radians
+    double sin_value = sin(angle_radians);
+    printf("Le sinus de %.2lf degrés est %.2lf\n", angle_degrees, sin_value);
+
+    // Exemple 4 : Exponentielle
+    double nombre3 = 2.0;
+    double exp_value = exp(nombre3);
+    printf("L'exponentielle de %.2lf est %.2lf\n", nombre3, exp_value);
+
+    // Exemple 5 : Logarithme naturel
+    double nombre4 = 10.0;
+    double log_value = log(nombre4);
+    printf("Le logarithme naturel de %.2lf est %.2lf\n", nombre4, log_value);
+
+    // Exemple 6 : Logarithme en base 10
+    double nombre5 = 100.0;
+    double log10_value = log10(nombre5);
+    printf("Le logarithme en base 10 de %.2lf est %.2lf\n", nombre5, log10_value);
+
+    // Exemple 7 : Puissance
+    double base = 2.0;
+    double exposant = 3.0;
+    double power_value = pow(base, exposant);
+    printf("%.2lf élevé à la puissance %.2lf est %.2lf\n", base, exposant, power_value);
+
+    // Exemple 8 : Arrondi
+    double nombre6 = 2.6;
+    double rounded_value = round(nombre6);
+    printf("L'arrondi de %.2lf est %.2lf\n", nombre6, rounded_value);
+
+    // Exemple 9 : Valeur maximale et minimale
+    double x = 5.0;
+    double y = 3.0;
+    double max_value = fmax(x, y);
+    double min_value = fmin(x, y);
+    printf("La valeur maximale entre %.2lf et %.2lf est %.2lf\n", x, y, max_value);
+    printf("La valeur minimale entre %.2lf et %.2lf est %.2lf\n", x, y, min_value);
+
+    // Exemple 10 : Valeur absolue (flottant)
+    double nombre7 = -3.14;
+    double abs_value = fabs(nombre7);
+    printf("La valeur absolue de %.2lf est %.2lf\n", nombre7, abs_value);
+
+    return 0;
+}
+
+
 }
